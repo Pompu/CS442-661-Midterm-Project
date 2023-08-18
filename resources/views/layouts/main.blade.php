3ca527e06491e1103da429f3b9da4df7d1a3d72e
Petchgamon Sangnual <petchgamon.s@ku.th>
2023-08-18 21:18:33 +0700

    Update view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,9 +10,10 @@
 </head>
 
 <body>
-
     @include('layouts.subviews.navbar')
-
+    <main class="bg-gray-100 mt-2 p-4 min-h-screen">
+        @yield('content')
+        </main>
 </body>
 
 </html>
